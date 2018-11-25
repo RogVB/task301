5f242329b144e5231d7ce773b6bac4ad36999eb2
Roger Vallejo <rogerbalbuena@hotmail.com>
2018-11-25 20:34:59 +0100

    Initial commit

---
+++ app/src/main/java/es/usj/task301/MainActivity.java
@@ -0,0 +1,52 @@
+package es.usj.task301;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private int corx, cory;
+    private Lienzo fondo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        corx = 100;
+        cory = 100;
+        ConstraintLayout layout1 =  findViewById(R.id.layout1);
+        fondo = new Lienzo(this);
+        fondo.setOnTouchListener(this);
+        layout1.addView(fondo);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        corx = (int) event.getX();
+        cory = (int) event.getY();
+        fondo.invalidate();
+        return true;
+
+    }
+
+    class Lienzo extends View {
+        public Lienzo(Context context) {
+            super(context);
+        }
+        protected void onDraw(Canvas canvas) {
+            canvas.drawRGB(255, 255, 0);
+            Paint pincel1 = new Paint();
+            pincel1.setARGB(255, 255, 0, 0);
+            pincel1.setStrokeWidth(4);
+            pincel1.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(corx, cory, 20, pincel1);
+        }
+    }
+}
